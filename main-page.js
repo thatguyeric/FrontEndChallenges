4bccf54ecb6c28af6a153db1619f91af08741220
Eric Marcondes <eric.marcondes@wallawalla.edu>
2018-10-10 13:52:10 -0400

    added detailed comments

--- main-page.js
+++ main-page.js
@@ -1,17 +1,29 @@
-$(document).ready(function () {
-    $('#correctAlert').hide();
-    $('#incorrectAlert').hide();
 
-    
+// hide alerts before DOM loads
+$('#correctAlert').hide();
+$('#incorrectAlert').hide();
+
+// Once DOM loads
+$(document).ready(function () {
+
     $("#checkAnswer").click(function () {
-        // compare user answer to txt file answer
-        // maybe we should use a nav bar and it will pull from a specific file based on the tab selected
+
+        // get the text from challenges.txt and put into 'answer' object
         $.get('challenges.txt', function (answer) {
+
+            // compare user answer to txt file answer
             if ($("#userInput").val() == answer) {
 
                 // show correct alert and hide incorrect alert
                 $('#correctAlert').fadeIn();
                 $('#incorrectAlert').hide();
+
+                // maybe we should use a nav bar and it will pull from a specific file based on the tab selected
+                // OR have the challenges seperated by *
+                // read the entire file into a variable 
+                // use another variable to get the text up to the *
+                // when next is pressed, the next challenge is pulled 
+
             } else {
 
                 // show incorrect alert and hide correct alert
@@ -21,25 +33,30 @@ $(document).ready(function () {
         }, 'text');
     });
 
-    // deletes content in the textarea
+
     $("#clearAnswer").click(function () {
+
+        // deletes content in the textarea
         $("#userInput").val("");
 
         // hide alerts
-        $('#correctAlert').hide();
-        $('#incorrectAlert').hide();
+        $('#correctAlert').fadeOut();
+        $('#incorrectAlert').fadeOut();
     });
 
     $("#nextChallenge").click(function () {
+
+        // deletes content in the textarea
         $("#userInput").val("");
 
         // hide alerts
-        $('#correctAlert').hide();
-        $('#incorrectAlert').hide();
-        
+        $('#correctAlert').fadeOut();
+        $('#incorrectAlert').fadeOut();
+
         /* 
         get the next challenge from text file and display to #challengeQuestion
         */
+
     });
 
 
