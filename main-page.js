2a1cec4528b1c9833946a0f2d424399ca48d4293
Eric Marcondes <eric.marcondes@wallawalla.edu>
2018-10-11 07:14:34 -0400

    added files and put their content into lists

--- main-page.js
+++ main-page.js
@@ -1,5 +1,11 @@
 var answer;
 var question;
+var JSquestions = [];
+var JSanswers = [];
+var HTMLquestions = [];
+var HTMLanswers = [];
+var CSSquestions = [];
+var CSSanswers = [];
 
 // hide alerts before DOM loads
 $('#correctAlert').hide();
@@ -8,64 +14,26 @@ $('#incorrectAlert').hide();
 // Once DOM loads
 $(document).ready(function () {
 
-    // sets up editor and enables preview
-    var editor = ace.edit("editor");
-    editor.session.setMode("ace/mode/html");
-    editor.getSession().on('change', function () {
-        $('#preview').contents().find('body').html(editor.getSession().getValue());
-    });
-
-    // enables smooth scrolling to element
-    $('a[href*="#"]').on('click', function (e) {
-        e.preventDefault();
-
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 500, 'linear');
-    });
-
-    $("#JSC1").click(function () {
-
-        // hide alerts
-        $('#incorrectAlert').hide();
-        $('#correctAlert').hide();
-
-        // get question and display it
-        $.get('js-challenge.txt', function (q) {
-            $("#challengeQuestion").text(q);
-        }, 'text');
-
-    });
+    /*******************
+     *     buttons
+     * *****************/
 
     $("#checkAnswer").click(function () {
 
-        // get the text from txt file and put into 'answer' object
-        $.get('js-answers.txt', function (answer) {
+        // compare user answer to txt file answer
+        /*if (editor.getValue() == JSanswers[0]) {
 
-            // compare user answer to txt file answer
-            if (editor.getValue() == answer) {
+            // show correct alert and hide incorrect alert
+            $('#correctAlert').fadeIn();
+            $('#incorrectAlert').hide();
 
-                // show correct alert and hide incorrect alert
-                $('#correctAlert').fadeIn();
-                $('#incorrectAlert').hide();
+        } else {
 
-                // maybe we should use a nav bar and it will pull from a specific file based on the tab selected
-                // OR have the challenges seperated by *
-                // read the entire file into a variable 
-                // use another variable to get the text up to the *
-                // when next is pressed, the next challenge is pulled 
-                // OR we can just create a drop down menu on each of the tabs
-                // and each question could load in from its own file ?
-
-            } else {
-
-                // show incorrect alert and hide correct alert
-                $('#incorrectAlert').fadeIn();
-                $('#correctAlert').hide();
-            }
-        }, 'text');
+            // show incorrect alert and hide correct alert
+            $('#incorrectAlert').fadeIn();
+            $('#correctAlert').hide();
+        } */
     });
-
 
     $("#clearAnswer").click(function () {
 
@@ -76,5 +44,75 @@ $(document).ready(function () {
         $('#correctAlert').fadeOut();
         $('#incorrectAlert').fadeOut();
     });
+
+    /*******************
+     *     tabs
+     * *****************/
+    
+    $("#JSC1").click(function () {
+
+        // hide alerts
+        $('#incorrectAlert').hide();
+        $('#correctAlert').hide();
+
+        //display question
+        $("#challengeQuestion").text(JSquestions[0]);
+
+    });
+
+
+    // sets up editor and enables preview
+    var editor = ace.edit("editor");
+    editor.session.setMode("ace/mode/html");
+    editor.getSession().on('change', function () {
+        $('#preview').contents().find('body').html(editor.getSession().getValue());
+    });
+
+    // enables smooth scrolling to elements
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
+
+    // pulls all questions and answers into lists
+    for (var i = 1; i < 4; i++) {
+        var aFile = 'challenges/js-answers' + i + '.txt';
+        $.get(aFile, function (a) {
+            JSanswers.push(a);
+        });
+
+        var cFile = 'challenges/js-challenge' + i + '.txt';
+        $.get(cFile, function (q) {
+            JSquestions.push(q);
+        });
+
+        aFile = 'challenges/css-answers' + i + '.txt';
+        $.get(aFile, function (a) {
+            CSSanswers.push(a);
+        });
+
+        cFile = 'challenges/css-challenge' + i + '.txt';
+        $.get(cFile, function (q) {
+            CSSquestions.push(q);
+        });
+
+        aFile = 'challenges/html-answers' + i + '.txt';
+        $.get(aFile, function (a) {
+            HTMLanswers.push(a);
+        });
+        
+        cFile = 'challenges/html-challenge' + i + '.txt';
+        $.get(cFile, function (q) {
+            HTMLquestions.push(q);
+        });
+    }
+
+    console.log('answers, questions');
+    console.log(JSanswers, JSquestions);
+    console.log(HTMLanswers, HTMLquestions);
+    console.log(CSSanswers, CSSquestions);
 
 });
