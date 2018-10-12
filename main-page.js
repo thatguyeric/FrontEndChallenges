aff19d1ed9d639ca0716fedcad8385081ae17afa
Eric Marcondes <eric.marcondes@wallawalla.edu>
2018-10-12 11:44:14 -0400

    refactored code

--- main-page.js
+++ main-page.js
@@ -1,35 +1,33 @@
 var answer;
 var question;
 
-// the first one in each is for HTML challenge 1
-// the second one in each is for CSS challenge 1
-// the third one in each is for JS challenge 1
 var JSchallenges = [];
 var CSSchallenges = [];
 var HTMLchallenges = [];
 var challengeQuestions = [];
+var HTMLanswers = [];
+var CSSanswers = [];
+var JSanswers = [];
 
-// var HTMLanswers = [];
-// var CSSanswers = [];
-// var JSanswers = [];
-
-getChallenges();
-getAnswers();
-
-// hide alerts before DOM loads
-$('#correctAlert').hide();
-$('#incorrectAlert').hide();
-
-// sets up editor and enables preview
 var editorHTML = ace.edit("editorHTML");
 editorHTML.session.setMode("ace/mode/html");
+
 var editorCSS = ace.edit("editorCSS");
 editorCSS.session.setMode("ace/mode/css");
+
 var editorJS = ace.edit("editorJS");
 editorJS.session.setMode("ace/mode/javascript");
+
 editorHTML.setReadOnly(true);
 editorCSS.setReadOnly(true);
 editorJS.setReadOnly(true);
+
+getChallengeQuestions();
+getHTMLchallenges();
+getJSchallenges();
+getCSSchallenges();
+getAnswers();
+hideAlerts();
 
 // Once DOM loads
 $(document).ready(function () {
@@ -39,10 +37,8 @@ $(document).ready(function () {
      * *********************************************************************************/
 
     $("#HTMLC1").click(function () {
-      
-        // hide alerts
-        $('#incorrectAlert').hide();
-        $('#correctAlert').hide();
+
+        hideAlerts();
 
         //display question and set read only for appropriate editors
         $("#challengeQuestion").text(challengeQuestions[0]);
@@ -51,21 +47,17 @@ $(document).ready(function () {
         editorJS.setReadOnly(true);
 
         // clear text in editors
-        editorHTML.setValue("");
-        editorCSS.setValue("");
-        editorJS.setValue("");
+        clearAllEditors();
 
         // display the html and css for this challenge
-        editorJS.setValue(JSchallenges[0]);
-        editorCSS.setValue(CSSchallenges[0]);
+        editorCSS.setValue(HTMLchallenges[0]);
+        editorJS.setValue(HTMLchallenges[1]);
 
     });
 
     $("#CSSC1").click(function () {
 
-        // hide alerts
-        $('#incorrectAlert').hide();
-        $('#correctAlert').hide();
+        hideAlerts();
 
         //display question and set read only for appropriate editors
         $("#challengeQuestion").text(challengeQuestions[1]);
@@ -74,21 +66,17 @@ $(document).ready(function () {
         editorJS.setReadOnly(true);
 
         // clear text in editors
-        editorHTML.setValue("");
-        editorCSS.setValue("");
-        editorJS.setValue("");
+        clearAllEditors();
 
-        // display the html and css for this challenge
-        editorJS.setValue(JSchallenges[1]);
-        editorHTML.setValue(HTMLchallenges[1]);
+        // display the html and JS for this challenge
+        editorHTML.setValue(CSSchallenges[0]);
+        editorJS.setValue(CSSchallenges[1]);
 
     });
 
     $("#JSC1").click(function () {
 
-        // hide alerts
-        $('#incorrectAlert').hide();
-        $('#correctAlert').hide();
+        hideAlerts();
 
         //display question and set read only for appropriate editors
         $("#challengeQuestion").text(challengeQuestions[2]);
@@ -97,13 +85,11 @@ $(document).ready(function () {
         editorCSS.setReadOnly(true);
 
         // clear text in editors
-        editorHTML.setValue("");
-        editorCSS.setValue("");
-        editorJS.setValue("");
+        clearAllEditors();
 
         // display the html and css for this challenge
-        editorHTML.setValue(HTMLchallenges[2]);
-        editorCSS.setValue(CSSchallenges[2]);
+        editorHTML.setValue(JSchallenges[0]);
+        editorCSS.setValue(JSchallenges[1]);
     });
 
     /*********************************************************************************
@@ -130,11 +116,6 @@ $(document).ready(function () {
 
     $("#clearAnswer").click(function () {
 
-        // deletes content in the editor
-        editorHTML.setValue("");
-        editorCSS.setValue("");
-        editorJS.setValue("");
-
         // hide alerts
         $('#correctAlert').fadeOut();
         $('#incorrectAlert').fadeOut();
@@ -143,43 +124,22 @@ $(document).ready(function () {
 });
 
 // pulls all questions and answers into lists
-function getChallenges() {
+function getAnswers() {
     $.get('challenges/HTMLchallenge1-html.txt', function (a) {
-        HTMLchallenges.push(a);
-    });
-
-    $.get('challenges/CSSchallenge1-html.txt', function (a) {
-        HTMLchallenges.push(a);
-    });
-
-    $.get('challenges/JSchallenge1-html.txt', function (a) {
-        HTMLchallenges.push(a);
-    });
-
-    $.get('challenges/HTMLchallenge1-css.txt', function (a) {
-        CSSchallenges.push(a);
-    });
-
-    $.get('challenges/CSSchallenge1-css.txt', function (a) {
-        CSSchallenges.push(a);
-    });
-
-    $.get('challenges/JSchallenge1-css.txt', function (a) {
-        CSSchallenges.push(a);
-    });
-
-    $.get('challenges/HTMLchallenge1-js.txt', function (a) {
-        JSchallenges.push(a);
-    });
-
-    $.get('challenges/CSSchallenge1-js.txt', function (a) {
-        JSchallenges.push(a);
+        HTMLanswers.push(a);
     });
 
     $.get('challenges/JSchallenge1-js.txt', function (a) {
-        JSchallenges.push(a);
+        JSanswers.push(a);
     });
 
+    $.get('challenges/CSSchallenge1-css.txt', function (a) {
+        CSSanswers.push(a);
+    });
+
+}
+
+function getChallengeQuestions() {
     $.get('challenges/HTMLchallenge1-question.txt', function (a) {
         challengeQuestions.push(a);
     });
@@ -193,27 +153,43 @@ function getChallenges() {
     });
 }
 
-function getAnswers(){
-    $.get('answers/HTMLanswers.txt', function (a) {
-        HTMLanswers.push(a);
+function getCSSchallenges() {
+    $.get('challenges/CSSchallenge1-html.txt', function (a) {
+        CSSchallenges.push(a);
     });
 
-    $.get('answers/CSSanswers.txt', function (a) {
-        HTMLanswers.push(a);
-    });
-
-    $.get('answers/JSanswers.txt', function (a) {
-        HTMLanswers.push(a);
+    $.get('challenges/CSSchallenge1-js.txt', function (a) {
+        CSSchallenges.push(a);
     });
 }
 
-function setQuestion(questionID){
+function getHTMLchallenges() {
+    $.get('challenges/HTMLchallenge1-css.txt', function (a) {
+        HTMLchallenges.push(a);
+    });
+
+    $.get('challenges/HTMLchallenge1-js.txt', function (a) {
+        HTMLchallenges.push(a);
+    });
+}
+
+function getJSchallenges() {
+    $.get('challenges/JSchallenge1-html.txt', function (a) {
+        JSchallenges.push(a);
+    });
+
+    $.get('challenges/JSchallenge1-css.txt', function (a) {
+        JSchallenges.push(a);
+    });
+}
+
+function setQuestion(questionID) {
     setEditorFromQuestionID(questionID);
     let currentEditor = getCurrentEditor();
     document.getElementById(currentEditor).setAttribute('current-question', questionID);
 }
 
-function setEditorFromQuestionID(questionID){
+function setEditorFromQuestionID(questionID) {
     let editor = "";
 
     if (questionID == "HTMLC1")
@@ -226,42 +202,58 @@ function setEditorFromQuestionID(questionID){
     document.getElementById("editor-container").setAttribute('current-editor', editor);
 }
 
-function getCurrentEditor(){
+function getCurrentEditor() {
     return document.getElementById("editor-container").getAttribute('current-editor')
 }
 
-function getCurrentQuestion(currentEditor){
+function getCurrentQuestion(currentEditor) {
     return document.getElementById(currentEditor).getAttribute('current-question');
 }
 
-function getUserAnswer(){
+function getUserAnswer() {
     let currentEditor = getCurrentEditor();
-    if(currentEditor == 'editorHTML')
+    if (currentEditor == 'editorHTML')
         return editorHTML.getValue().replace(/\s/g, '');
-    else if(currentEditor == 'editorCSS')
+    else if (currentEditor == 'editorCSS')
         return editorCSS.getValue().replace(/\s/g, '');
-    else if(currentEditor == 'editorJS')
+    else if (currentEditor == 'editorJS')
         return editorJS.getValue().replace(/\s/g, '');
     return document.getElementById(currentEditor).innerText;
 }
 
-function checkAnswer(){
+function checkAnswer() {
     let currentEditor = getCurrentEditor();
     let currentQuestion = getCurrentQuestion(currentEditor);
     let correctAnswer = getCorrectAnswer(currentQuestion);
     let userAnswer = getUserAnswer();
 
-    if(userAnswer === correctAnswer)
-        alert('you gucci');
+    if (userAnswer === correctAnswer) {
+        $('#correctAlert').fadeIn();
+        $('#incorrectAlert').hide();
+    } else {
+        $('#correctAlert').hide();
+        $('#incorrectAlert').fadeIn();
+    }
 }
 
-function getCorrectAnswer(question){
-    let answerIndex = parseInt(question.charAt(question.length-1) - 1);
-    if(question == "HTMLC1"){
+function getCorrectAnswer(question) {
+    let answerIndex = parseInt(question.charAt(question.length - 1) - 1);
+    if (question == "HTMLC1") {
         return HTMLanswers[answerIndex].replace(/\s/g, '');
-    } else if(question == "CSSC1") {
+    } else if (question == "CSSC1") {
         return CSSanswers[answerIndex].replace(/\s/g, '');
-    } else if(question == "JSC1"){
+    } else if (question == "JSC1") {
         return JSanswers[answerIndex].replace(/\s/g, '');
     }
+}
+
+function clearAllEditors() {
+    editorHTML.setValue("");
+    editorCSS.setValue("");
+    editorJS.setValue("");
+}
+
+function hideAlerts() {
+    $('#incorrectAlert').hide();
+    $('#correctAlert').hide();
 }
